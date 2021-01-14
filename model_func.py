d36e821d4b522f2405e5924a5f775d223564c0fe
nkmashaev <nikita_mashaev@mail.ru>
2021-01-14 18:40:17 +0300

    Part merging

---
+++ model_func.py
@@ -0,0 +1,52 @@
+from typing import Tuple
+
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import img_to_array
+
+
+def standardize_img(img: Image) -> Image:
+    """
+    standardize_img function takes PIL.Image object as input
+    and create new PIL.Image object from this with a size=(224,224),
+    format="PNG" and mode "RGB"
+
+    :param img: original PIL.Image object
+    :return: reformatted PIL.Image object
+    """
+    reformatted_img = img.resize((224, 224)).convert(mode="RGB")
+    return reformatted_img
+
+
+def load_tf_model(path: str) -> keras.Model:
+    """
+    Load serialized keras model from the directory \`path\`
+
+    :param path: directory with serialized keras model
+    :return: serialized keras model
+    """
+    print(f"Loading keras model from {path}")
+    return keras.models.load_model(path)
+
+
+def is_cat(model: keras.Model, img: Image) -> bool:
+    """
+    Returns True if Cat on the picture, Dog otherwise
+
+    :param model: Loaded model for CatVSDog classification
+    :param img: PIL PNG image with size 224x224
+        example:
+        >>> print(img)
+        <PIL.PngImagePlugin.PngImageFile image mode=RGB size=224x224 at ...>
+    :return:
+    """
+    img_array = tf.cast(img_to_array(img), tf.float32) / 255.0
+    img_expended = np.expand_dims(img_array, axis=0)
+    return model.predict(img_expended)[0][0] < 0.5
+
+
+def classify(model: keras.Model, img: Image):
+    img = standardize_img(img)
+    return img, is_cat(model, img)
---
+++ test/test_img.py
@@ -0,0 +1,34 @@
+import os
+
+import pytest
+from PIL import Image
+
+from model_func import standardize_img
+
+
+# img.standardize_img tests
+@pytest.fixture
+def test_img_list():
+    test_dir = os.path.join(os.path.dirname(__file__), "test_images")
+    test_files = [os.path.join(test_dir, path) for path in os.listdir(test_dir)]
+    test_imgs = []
+    for name in test_files:
+        try:
+            img = Image.open(name)
+        except IOError:
+            print(f"Error: {name} is not image!")
+            continue
+        test_imgs.append(img)
+
+    yield test_imgs
+
+    while test_imgs:
+        img = test_imgs.pop()
+        img.close()
+
+
+def test_standardize_img(test_img_list):
+    for img in test_img_list:
+        img = standardize_img(img)
+        assert img.size == (224, 224)
+        assert img.mode == "RGB"
